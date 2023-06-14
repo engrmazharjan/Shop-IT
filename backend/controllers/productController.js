62a0a92ff0cc63990cfc6c08bbeeb005aeb5a049
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-14 18:34:55 +0500

    Admin - Update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-// * Create New Product => /api/v1/product/new
+// * Create New Product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -28,6 +28,28 @@ exports.getSingleProduct = async (req, res, next) => {
       message: "Product Not Found!",
     });
   }
+  res.status(200).json({
+    sucess: true,
+    product,
+  });
+};
+
+// * Update Product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Product Not Found!",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
   res.status(200).json({
     sucess: true,
     product,
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,11 +4,14 @@ const {
   newProduct,
   getProducts,
   getSingleProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
+
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
