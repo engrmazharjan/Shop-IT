6699f19b3287b9483383ac6b4a82cf7ede98fd2c
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-14 15:05:28 +0500

    Connecting To MongoDB Database

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 // * Import All Routes
 const products = require("./routes/product");
 
+// * Use All Routes
 app.use("/api/v1", products);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 // * Setting up config file
 dotenv.config({ path: "backend/config/config.env" });
+
+// * Conneting To Database
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server Listening on PORT:${process.env.PORT} in ${process.env.NODE_ENV} mode`
