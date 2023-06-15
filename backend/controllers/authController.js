0e1fcee389df09164f73ddd170216ccec5f36241
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-15 17:31:04 +0500

    Login User & Assign Token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,3 +23,33 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     token,
   });
 });
+
+// * Login User => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // * Check If Email And Password Is Entered By User
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
+
+  // * Finding User In Database
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email Or Password", 401));
+  }
+
+  // * Check If Password Is Correct Or Not
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email Or Password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
