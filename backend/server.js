21419f1defa148d48c65f3746a80c5d1953dfe6f
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-15 12:56:02 +0500

    Handling Undandled Promise Rejection

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,17 @@ dotenv.config({ path: "backend/config/config.env" });
 // * Conneting To Database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server Listening on PORT:${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
+});
+
+// * Handle Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting Down The Server Due To Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
