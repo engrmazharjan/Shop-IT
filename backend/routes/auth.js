98017fb537d16683255970791e5e0f4c8ff2adb2
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-16 17:04:34 +0500

    Show User Profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,14 +6,18 @@ const {
   loginUser,
   forgotPassword,
   resetPassword,
+  getUserProfile,
   logout,
 } = require("../controllers/authController");
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 router.route("/logout").get(logout);
 
