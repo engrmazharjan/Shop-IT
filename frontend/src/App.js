f36411aa3c5ce516cf4f43a740bab94acf5107ed
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-19 18:50:03 +0500

    Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Shop IT</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
