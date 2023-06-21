e2ea66601a58b5ff7237bc973e451029dcc5e58f
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-21 14:52:26 +0500

    Shop-IT Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
